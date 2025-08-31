0957e8e220bff1391fbf6ac6d1cb0b83539842b5
amihsan <rumi.apece.du@gmail.com>
2025-08-31 13:41:34 +0200

    test

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,7 +3,12 @@
 # Generate the env-config.js file for runtime environment variables
 cat <<EOF > /usr/share/nginx/html/env-config.js
 window._env_ = {
-  VITE_API_URL: "${VITE_API_URL}"
+  VITE_API_URL: "${VITE_API_URL}",
+  VITE_AWS_REGION: "${VITE_AWS_REGION}",
+  VITE_AWS_USER_POOL_ID: "${VITE_AWS_USER_POOL_ID}",
+  VITE_AWS_APP_CLIENT_ID: "${VITE_AWS_APP_CLIENT_ID}",
+  VITE_AWS_IDENTITY_POOL_ID: "${VITE_AWS_IDENTITY_POOL_ID}"
+  VITE_AWS_COGNITO_REGION: "${VITE_AWS_COGNITO_REGION}"
 };
 EOF
 
